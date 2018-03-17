678fad11ea247709dcf809e79aeeaa4805aefd92
Jurgis <jurgis.kargaudas@gmail.com>
2018-03-17 23:00:05 +0200

    Extract and display name from token.

--- our-happy-client/src/App.js
+++ our-happy-client/src/App.js
@@ -6,12 +6,30 @@ class App extends Component {
 
   appId = "our-happy-client";
   uniSignBankViewUrl = "http://localhost:61392/api/BankView/" + this.appId;
+  userInfoUrl = "http://localhost:61392/api/BankView/UserInfo/";
 
   constructor() {
     super();
     this.state = {
-      bankTemplate: ""
+      bankTemplate: "",
+      greeting: null
     }
+    this.fetchUserInfo = this.fetchUserInfo.bind(this);
+  }
+
+  fetchUserInfo(userToken) {
+    fetch(this.userInfoUrl + userToken)
+      .then((res) => res.json())
+      .then((body) => this.setState({ greeting: "Hello there, " + body.fullName + "!"}));
+  }
+
+  componentDidMount() {
+    const search = this.props.location.search;
+    if (search) {
+      const params = new URLSearchParams(search);
+      const userToken = params.get('token');
+      this.fetchUserInfo(userToken);
+    } 
   }
 
   componentWillMount() {
@@ -30,7 +48,9 @@ class App extends Component {
           <h1 className="App-title">Very Important Business Consumer</h1>
         </header>
         <div dangerouslySetInnerHTML={{__html: this.state.bankTemplate}}></div>
-        
+        <h5>
+          {this.state.greeting}
+        </h5>
 
       </div>
     );
